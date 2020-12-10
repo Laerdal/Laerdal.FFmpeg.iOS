b9008840d41026bb32b807557fbf3557e89d3ee9
Francois Raminosona <francois.raminosona@laerdal.com>
2020-12-10 02:26:12 +0100

    Forgot Audio package

--- build.sh
+++ build.sh
@@ -138,6 +138,7 @@ generate_sharpie_files()
 
 
 # see https://github.com/tanersener/mobile-ffmpeg for more information
+time build_package audio Audio
 time build_package full Full
 time build_package full-gpl Full.Gpl
 time build_package https Https
